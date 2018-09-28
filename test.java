319ffe986eae56723b3536dbc97f214a86ddfb90
arjunsingha3spl <43671053+arjunsingha3spl@users.noreply.github.com>
2018-09-28 15:31:02 +0530

    test.java
    BigDecimal code

---
+++ test.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.math.BigDecimal;
+
+public class gotohell {
+	public static void main(String[] args) {
+		BigDecimal a = new BigDecimal(12);
+		BigDecimal b = new BigDecimal(-120);
+		BigDecimal c= new BigDecimal(0.0);
+		c= a.add(b);
+		System.out.println("c  " +c);
+	}
